d43b090f0cf4bef6e0cf296a3fbaf620d1a05d58
Imran Brown <imraa2@yahoo.com>
2026-07-28 11:15:54 -0300

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.py
@@ -0,0 +1,23 @@
+from collections import Counter
+import heapq
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # count module
+        ans_list = Counter(nums)
+        return [ key for key,value in ans_list.most_common(k)]
+        # --------------------------------------------
+        # or loop through make map of num -> freq
+            # dict_ = {}
+            # for num in nums:
+            #     dict_[num] = 1 + dict_.get(num,0)
+        # sort
+            # sorted_keys = sorted(dict_, key=dict_.get, reverse=True)
+            # return sorted_keys[0:k]
+        # min_heap
+        # freq = {}
+        # for num in nums:
+        #     freq[num] = 1 + freq.get(num,0)
+        # return [num for num, count in heapq.nlargest(k, freq.items(), key=lambda x: x[1])]
+
+        
